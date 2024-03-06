a13fbff8e442678a1d600155a7336c4d263504a0
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-03-06 16:52:51 +0530

    Add files via upload

---
+++ DoWhileLoop2.java
@@ -0,0 +1,12 @@
+class DoWhileLoop2
+ {
+  public static void main(String[]args)
+   {
+     int v=1;
+     do
+     {
+      System.out.println(v*2);
+      v++;
+     }while(v<=10);
+   }
+ }//2,4,6,8,10,12,14,16,18,20
